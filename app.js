07ec21079b77bf616d2d83bb1ded599deaf1e455
jose luis ramos dorado <vikingofdeath@yahoo.com>
2021-09-23 01:34:14 -0500

    String to integer

--- app.js
+++ app.js
@@ -220,4 +220,65 @@ const cakes = (recipe, available) => {
 	}, 0);
 };
 
-cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
+// cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 });
+
+const base = new Map([
+	["zero", 0],
+	["one", 1],
+	["two", 2],
+	["three", 3],
+	["four", 4],
+	["five", 5],
+	["six", 6],
+	["seven", 7],
+	["eight", 8],
+	["nine", 9],
+	["ten", 10],
+	["eleven", 11],
+	["twelve", 12],
+	["thirteen", 13],
+	["fourteen", 14],
+	["fifteen", 15],
+	["sixteen", 16],
+	["seventeen", 17],
+	["eighteen", 18],
+	["nineteen", 19],
+	["twenty", 20],
+	["thirty", 30],
+	["forty", 40],
+	["fifty", 50],
+	["sixty", 60],
+	["seventy", 70],
+	["eighty", 80],
+	["ninety", 90],
+	["hundred", 100],
+	["thousand", 1000],
+	["million", 1000000],
+]);
+
+const parseInt = (string) => {
+	const acumulate = string
+		.trim()
+		.toLowerCase()
+		.split(" and")
+		.join("")
+		.split(" ")
+		.map((word) =>
+			!word.includes("-") ? base.get(word) : word.split("-").reduce((acc, number) => (acc += base.get(number)), 0)
+		)
+		.reduce(
+			(acc, number) => {
+				number % 100 === 0 ? (acc.count *= number) : (acc.count += number);
+				if (number > 100) {
+					acc.carring += acc.count;
+					acc.count = 0;
+				}
+				return acc;
+			},
+			{ count: 0, carring: 0 }
+		);
+	// console.log(acumulate);
+	return acumulate.count + acumulate.carring;
+};
+
+// parseInt("four hundred fifty-six thousand seven hundred eighty-nine");
